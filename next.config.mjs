f8ebd710a4894f87f163765c3c0ea96d1651ba3a
25rae25 <whdudfo0124@gmail.com>
2024-06-13 23:21:38 +0900

    config메모

--- next.config.mjs
+++ next.config.mjs
@@ -6,3 +6,18 @@ const nextConfig = {
 };
 
 export default nextConfig;
+
+// next.config.js
+// 바뀐방식
+// module.exports = {
+//   images: {
+//     remotePatterns: [
+//       {
+//         protocol: 'https',
+//         hostname: 'example.com',
+//         port: '', // 포트가 있다면 여기에 명시하세요.
+//         pathname: '/**', // 경로 패턴을 지정합니다. '/**'는 모든 경로를 허용합니다.
+//       },
+//     ],
+//   },
+// };
