45fe5b2584aab1b41ab20bcce6a131e14abe9f8c
25rae25 <whdudfo0124@gmail.com>
2024-06-13 22:58:28 +0900

    exploreContainer 마크업

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import CarouselContainer from "@/container/CarouselContainer/CarouselContainer";
 import NavigateContainer from "@/container/NavigateContainer/NavigateContainer";
 import MainPage from "./(page)/main/page";
+import ExploreContainer from "@/container/ExploreContainer/ExploreContainer";
 
 export default function HomePage() {
   return (
     <>
       <NavigateContainer />
+      <ExploreContainer />
       <CarouselContainer />
       <MainPage />
     </>
--- src/container/ExploreContainer/ExploreContainer.tsx
+++ src/container/ExploreContainer/ExploreContainer.tsx
@@ -1,7 +1,30 @@
+import Link from "next/link";
+
 export default function ExploreContainer() {
   return (
-    <>
-      <div>체험존</div>
-    </>
+    <div>
+      <div>
+        <Link href="/">
+          {/* 이미지 들어갈곳 */}
+          <p>얼리버드</p>
+        </Link>
+        <Link href="/">
+          {/* 이미지 들어갈곳 */}
+          <p>여자매치</p>
+        </Link>
+        <Link href="/">
+          {/* 이미지 들어갈곳 */}
+          <p>스타터</p>
+        </Link>
+        <Link href="/">
+          {/* 이미지 들어갈곳 */}
+          <p>챌린지</p>
+        </Link>
+        <Link href="/">
+          {/* 이미지 들어갈곳 */}
+          <p>시작하기</p>
+        </Link>
+      </div>
+    </div>
   );
 }
