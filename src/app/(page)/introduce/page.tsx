3f6f9e9481acdd99d23230c9d0373d36aad5045a
25rae25 <whdudfo0124@gmail.com>
2024-06-14 15:24:13 +0900

    IntroduceContainer 페이지

---
+++ src/app/(page)/introduce/page.tsx
@@ -0,0 +1,5 @@
+import IntroduceContainer from "@/container/IntroduceContainer/IntroduceContainer";
+
+export default function IntroducePage() {
+  return <IntroduceContainer />;
+}
---
+++ src/container/IntroduceContainer/IntroduceContainer.tsx
@@ -0,0 +1,7 @@
+export default function IntroduceContainer() {
+  return (
+    <>
+      <div>123</div>
+    </>
+  );
+}
