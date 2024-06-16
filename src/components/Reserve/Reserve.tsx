e6382d67207e13b45076d90382c9ccee8a796ba5
25rae25 <whdudfo0124@gmail.com>
2024-06-16 23:15:03 +0900

    Reserve 마크업중

--- src/components/Reserve/Reserve.tsx
+++ src/components/Reserve/Reserve.tsx
@@ -1,12 +1,24 @@
 "use client";
 
+import Image from "next/image";
 import * as S from "./ReserveStyles";
 
 export default function Reserve() {
   return (
     <S.ReserveWrap>
       <S.ReserveHeader></S.ReserveHeader>
-      <S.ReserveBody></S.ReserveBody>
+      <S.ReserveBody>
+        <div>
+          <ul>
+            <li>
+              {/* <Image></Image> */}
+              <div>6vs6 3파전</div>
+            </li>
+            <li>10 ~ 18명</li>
+            <li>풋살화/운동화</li>
+          </ul>
+        </div>
+      </S.ReserveBody>
     </S.ReserveWrap>
   );
 }
