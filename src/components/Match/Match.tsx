620611b0af29f1a1eb9bd15b02296c41850f49f8
25rae25 <whdudfo0124@gmail.com>
2024-06-18 22:02:39 +0900

    Match css 수정

--- src/components/Match/Match.tsx
+++ src/components/Match/Match.tsx
@@ -4,22 +4,32 @@ import * as S from "./MatchStyles";
 
 export default function Match() {
   return (
-    <S.MatchContainer>
-      <S.MatchTitle>매치 포인트</S.MatchTitle>
-      <S.MatchWrapper>
-        <S.MatchWrap>
-          <S.FieldIcon />
-          <S.IconTitle>6vs6 3파전</S.IconTitle>
-        </S.MatchWrap>
-        <S.MatchWrap>
-          <S.UserIcon />
-          <S.IconTitle>10~15명</S.IconTitle>
-        </S.MatchWrap>
-        <S.MatchWrap>
-          <S.ShoesIcon />
-          <S.IconTitle>풋살화/운동화</S.IconTitle>
-        </S.MatchWrap>
-      </S.MatchWrapper>
-    </S.MatchContainer>
+    <S.Section>
+      <S.MatchContainer>
+        <S.MatchTitle>매치 포인트</S.MatchTitle>
+        <S.MatchWrapper>
+          <S.MatchWrap>
+            <S.FieldIcon />
+            <S.IconTitle>6vs6 3파전</S.IconTitle>
+          </S.MatchWrap>
+          <S.MatchWrap>
+            <S.UserIcon />
+            <S.IconTitle>10~15명</S.IconTitle>
+          </S.MatchWrap>
+          <S.MatchWrap>
+            <S.ShoesIcon />
+            <S.IconTitle>풋살화/운동화</S.IconTitle>
+          </S.MatchWrap>
+        </S.MatchWrapper>
+      </S.MatchContainer>
+      <S.SubmitWrapper>
+        <S.SubmitWrap>
+          <S.MatchTime>6월 18일 화요일 18:00</S.MatchTime>
+          <S.MatchPlace>서울 교육대학교 운동장</S.MatchPlace>
+          <S.SubmitBar>궁금중을 모두 해결하세요</S.SubmitBar>
+          <S.SubmitButton>신청하기</S.SubmitButton>
+        </S.SubmitWrap>
+      </S.SubmitWrapper>
+    </S.Section>
   );
 }
--- src/components/Match/MatchStyles.ts
+++ src/components/Match/MatchStyles.ts
@@ -3,6 +3,10 @@ import { GiSoccerField } from "react-icons/gi";
 import { LuUsers } from "react-icons/lu";
 import { TbShoe } from "react-icons/tb";
 
+export const Section = styled.div`
+  display: flex;
+`;
+
 export const MatchContainer = styled.div`
   width: 60%;
   height: auto;
@@ -11,7 +15,7 @@ export const MatchContainer = styled.div`
 
 export const MatchWrapper = styled.div`
   padding: 20px;
-  display: flex;
+  display: inline-flex;
 `;
 
 export const MatchWrap = styled.div`
@@ -42,4 +46,49 @@ export const UserIcon = styled(LuUsers)`
 export const ShoesIcon = styled(TbShoe)`
   width: 24px;
   height: 24px;
+`;
+
+export const SubmitWrapper = styled.div`
+  width: 40%;
+  margin: 0 auto;
+  background-color: #fff;
+  margin-left: 10px;
+`;
+
+export const SubmitWrap = styled.div`
+  padding: 20px 10px;
+`;
+
+export const MatchTime = styled.div`
+  font-weight: 700;
+  font-size: 16px;
+  margin-bottom: 3px;
+`;
+
+export const MatchPlace = styled.div`
+  color: #282b33;
+  font-size: 22px;
+  font-weight: 400;
+  margin-bottom: 3px;
+`;
+
+export const SubmitBar = styled.div`
+  background-color: #00d281;
+  font-size: 12px;
+  text-align: center;
+  height: 32px;
+  line-height: 32px;
+  cursor: pointer;
+  color: #fff;
+`;
+
+export const SubmitButton = styled.button`
+  background-color: #1570ff;
+  color: #fff;
+  font-size: 18px;
+  font-weight: 400;
+  border: none;
+  border-radius: 6px;
+  padding: 12px;
+  margin: 20px 0;
 `;
