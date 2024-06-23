4183e2f04390d9d1dd38b09f4ef9c38823691163
25rae25 <whdudfo0124@gmail.com>
2024-06-23 22:10:52 +0900

    common Button component 제작

---
+++ src/components/common/Button/Button.tsx
@@ -0,0 +1,5 @@
+import * as S from "./ButtonStyles";
+
+export default function Button() {
+  return <Button />;
+}
---
+++ src/components/common/Button/ButtonStyles.ts
@@ -0,0 +1,33 @@
+import styled from "@emotion/styled";
+
+export const SubmitButton = styled.button`
+  background-color: #1570ff;
+  color: #fff;
+  text-align: center;
+  font-size: 16px;
+  border-radius: 12px;
+  font-weight: 500;
+  padding: 20px;
+  margin-top: 20px;
+  border: none;
+  width: 40%;
+  display: block;
+  margin: 0 auto;
+  cursor: pointer;
+`;
+
+export const EditButton = styled.button`
+  background-color: #1570ff;
+  color: #fff;
+  text-align: center;
+  font-size: 16px;
+  border-radius: 12px;
+  font-weight: 500;
+  padding: 20px;
+  /* margin-top: 20px; */
+  border: none;
+  width: 40%;
+  display: block;
+  margin: 0 auto;
+  cursor: pointer;
+`;
