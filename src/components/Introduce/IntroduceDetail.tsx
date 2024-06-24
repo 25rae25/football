cac85bd39f04d4845174e87c5307003259bcfc2f
25rae25 <whdudfo0124@gmail.com>
2024-06-24 16:21:03 +0900

    IntrodueceDetail teamData불러오는 값으로 수정

--- src/components/Introduce/IntroduceDetail.tsx
+++ src/components/Introduce/IntroduceDetail.tsx
@@ -19,36 +19,36 @@ export default function IntroduceDetail({ teamData }: ITeamdata) {
         </S.IntroduceDetail>
         <S.IntroduceDetail>
           <S.IntroduceDetailTitle>주요 활동지역</S.IntroduceDetailTitle>
-          <S.IntroduceDetailContent>서울특별시 동작구</S.IntroduceDetailContent>
+          <S.IntroduceDetailContent>
+            {teamData.province}
+          </S.IntroduceDetailContent>
         </S.IntroduceDetail>
         <S.IntroduceDetail>
           <S.IntroduceDetailTitle>운동 장소</S.IntroduceDetailTitle>
           <S.IntroduceDetailContent>
-            서울교육대학교 운동장
+            {teamData.address}
           </S.IntroduceDetailContent>
         </S.IntroduceDetail>
         <S.IntroduceDetail>
           <S.IntroduceDetailTitle>운동 시간</S.IntroduceDetailTitle>
-          <S.IntroduceDetailContent>
-            매주 일요일 오후 1~3시
-          </S.IntroduceDetailContent>
+          <S.IntroduceDetailContent>{teamData.time}</S.IntroduceDetailContent>
         </S.IntroduceDetail>
         <S.IntroduceDetail>
           <S.IntroduceDetailTitle>주요 연령대</S.IntroduceDetailTitle>
-          <S.IntroduceDetailContent>20~30대</S.IntroduceDetailContent>
+          <S.IntroduceDetailContent>{teamData.range}</S.IntroduceDetailContent>
         </S.IntroduceDetail>
         <S.IntroduceDetail>
           <S.IntroduceDetailTitle>회비</S.IntroduceDetailTitle>
-          <S.IntroduceDetailContent>20,000원</S.IntroduceDetailContent>
+          <S.IntroduceDetailContent>{teamData.fee}</S.IntroduceDetailContent>
         </S.IntroduceDetail>
         <S.IntroduceDetail>
           <S.IntroduceDetailTitle>대표번호</S.IntroduceDetailTitle>
-          <S.IntroduceDetailContent>010-2518-2688</S.IntroduceDetailContent>
+          <S.IntroduceDetailContent>{teamData.phone}</S.IntroduceDetailContent>
         </S.IntroduceDetail>
         <S.IntroduceDetail>
           <S.IntroduceDetailTitle>소개</S.IntroduceDetailTitle>
           <S.IntroduceDetailContent>
-            안녕하세요 FC서초팀 입니다.
+            {teamData.introduction}
           </S.IntroduceDetailContent>
         </S.IntroduceDetail>
       </S.IntroduceDetailWrap>
