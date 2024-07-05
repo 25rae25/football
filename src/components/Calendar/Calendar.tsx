f92435c26c030f845aa8811bb43a21a527669564
25rae25 <whdudfo0124@gmail.com>
2024-07-05 17:25:38 +0900

    Mypage Calendar css수정

--- src/components/Calendar/Calendar.tsx
+++ src/components/Calendar/Calendar.tsx
@@ -4,6 +4,7 @@ import { useState } from "react";
 import Calendar from "react-calendar";
 import "react-calendar/dist/Calendar.css";
 import * as S from "./CalendarStyles";
+import moment from "moment";
 
 type ValuePiece = Date | null;
 type Value = ValuePiece | [ValuePiece, ValuePiece];
@@ -11,14 +12,25 @@ type Value = ValuePiece | [ValuePiece, ValuePiece];
 export default function MyCalendar() {
   const [value, onChange] = useState<Value>(new Date());
 
+  const tileClassName = ({ date }) => {
+    if (date.getDay() === 0) {
+      return "sunday";
+    }
+    if (date.getDay() === 6) {
+      return "saturday";
+    }
+    return "";
+  };
   return (
     <>
       <S.StyleCalendar
         locale="ko"
         onChange={onChange}
+        tileClassName={tileClassName}
         value={value}
         calendarType="gregory"
         view="month"
+        formatDay={(locale, date) => moment(date).format("DD")}
         prev2Label={null}
         next2Label={null}
       />
--- src/components/Calendar/CalendarStyles.tsx
+++ src/components/Calendar/CalendarStyles.tsx
@@ -3,33 +3,103 @@ import Calendar from "react-calendar";
 
 export const StyleCalendar = styled(Calendar)`
   width: 100%;
+  border: none;
+  background-color: #fff;
 
   .react-calendar {
-    width: 100%;
   }
 
-  .react-calendar__navigation {
-    width: inherit;
-    height: 3.5rem;
+  /* 년-월 */
+  .react-calendar__navigation__label > span {
+    color: #000;
+    font-size: 20px;
+    font-weight: bold;
+    line-height: 140%;
+  }
+
+  .react-calendar__navigation button:enabled:hover {
+    color: #fff !important;
+    width: 5px;
+    background-color: #1570ff;
+    border-radius: 3px;
+  }
+
+  .react-calendar__month-view__weekdays__weekday--weekend {
+    color: #000;
+  }
+
+  /* 요일 */
+  .react-calendar__month-view__weekdays__weekday {
+    color: #000;
+    font-size: 18px;
+    text-decoration: none !important;
+    font-weight: bold;
+    margin-bottom: 10px;
+  }
+  .react-calendar__month-view__weekdays__weekday abbr {
+    text-decoration: none !important;
+  }
+
+  .react-calendar__tile.saturday {
+    color: #6560ff;
+  }
+
+  .react-calendar__tile.sunday {
+    color: red;
+  }
+
+  .react-calendar__month-view__days__day--weekend {
+    color: #000;
+    font-size: 18px;
+    width: 44px;
+    height: 44px;
+    text-align: center;
+  }
+
+  /* 이번 달 일자 */
+  .react-calendar__tile {
+    color: #000;
+    font-size: 18px;
     display: flex;
+    flex-direction: column;
     text-align: center;
     align-items: center;
-    padding: 0 5.5rem;
+    background-color: #fff;
+  }
 
-    .react-calendar__navigation__label {
-      width: 5.5rem;
-      height: 1.375rem;
-      font-size: 16px;
-      border: none;
-      font-weight: 700;
-      background-color: #fff;
-    }
+  /* 저번 달 & 다음 달 일자 */
+  .react-calendar__month-view__days__day--neighboringMonth {
+    color: #000;
+    font-size: 18px;
+    font-weight: 400;
+    width: 44px;
+    height: 44px;
+  }
 
-    .react-calendar__navigation__prev-button {
-      border: none;
-      background-color: #fff;
-      cursor: pointer;
-      opacity: 0;
-    }
+  /* 오늘 날짜 */
+  .react-calendar__tile--now {
+    background-color: #292929;
+    color: #fff;
+    font-size: 18px;
+    font-weight: bold;
+    border-radius: 50px;
+  }
+
+  /* 선택된 날짜의 배경색 변경 */
+  .react-calendar__tile--active {
+    background-color: #1570ff;
+    color: #fff;
+    font-size: 18px;
+    font-weight: bold;
+    border-radius: 50px;
+  }
+
+  .react-calendar__tile--hasActive {
+    background: #1570ff;
+  }
+
+  .react-calendar__tile--active:enabled:hover,
+  .react-calendar__tile--active:enabled:focus {
+    background: #1570ff;
   }
 `;
