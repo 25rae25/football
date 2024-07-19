a20ea965bffe076182c1132b2265c85206a1a77d
25rae25 <whdudfo0124@gmail.com>
2024-07-19 15:12:32 +0900

    git test

--- src/container/NavbarContainer/NavbarContainer.tsx
+++ src/container/NavbarContainer/NavbarContainer.tsx
@@ -15,16 +15,6 @@ export default function NavbarContainer() {
 
   const handleSearch = () => {};
 
-  // const handleKeyDown = (e: React.KeyboardEvent) => {
-  //   if (e.key === "enter") {
-  //     if (text === "") {
-  //       // input 태그 안 빈값 검색했을때  default value redux, recoil 중 사용
-  //     }
-  //   } else {
-  //     setSearch(text)
-  //   }
-  // };
-
   useEffect(() => {
     if (typeof window !== "undefined") {
       setUserToken(localStorage.getItem("accessToken"));
